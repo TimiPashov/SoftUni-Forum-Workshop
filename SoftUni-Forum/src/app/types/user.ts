30bcc692a08dd5ad3b759afa2f8d2b578f61cfa8
TimiPashov <pashov91@gmail.com>
2024-03-04 14:16:32 +0200

    implem User, Theme types

---
+++ SoftUni-Forum/src/app/types/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+   
+        "themes": String[],
+        "posts": String[],
+        "_id": string,
+        "tel": string,
+        "email": string,
+        "username": string,
+        "password": string,
+        "created_at": string,
+        "updatedAt": string,
+        "__v": number
+   
+}
