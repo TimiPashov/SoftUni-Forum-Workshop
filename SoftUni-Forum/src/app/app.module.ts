c32f84c4e107ec82f2b7b09cb55da823d4f2f0f7
TimiPashov <pashov91@gmail.com>
2024-03-04 13:26:36 +0200

    genereate main, themes-list, posts-list components

--- SoftUni-Forum/src/app/app.module.ts
+++ SoftUni-Forum/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
     BrowserModule,
