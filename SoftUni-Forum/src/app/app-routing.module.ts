4b7f0524ebc4992fd13a093f734a78c645e43ab2
TimiPashov <pashov91@gmail.com>
2024-03-11 14:21:53 +0200

    configure routes/links

--- SoftUni-Forum/src/app/app-routing.module.ts
+++ SoftUni-Forum/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: '/home' },
-{ path: 'home', component: HomeComponent },
-{ path: '**', redirectTo: '/404' },
-{ path: '404', component: ErrorComponent }
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/404' },
+  { path: '404', component: ErrorComponent }
 ];
 
 
