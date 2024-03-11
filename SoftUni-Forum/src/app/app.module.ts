368a851cad756ae377c998b9f3e917d0140182be
TimiPashov <pashov91@gmail.com>
2024-03-11 14:01:21 +0200

    add home component

--- SoftUni-Forum/src/app/app.module.ts
+++ SoftUni-Forum/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UserModule } from './user/user.module';
 import { ThemeModule } from './theme/theme.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
