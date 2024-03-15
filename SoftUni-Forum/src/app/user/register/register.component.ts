121c1b0999f7aaaa73e5d9d71fda14e8c72da923
TimiPashov <pashov91@gmail.com>
2024-03-15 13:28:21 +0200

    add validations on register form

--- SoftUni-Forum/src/app/user/register/register.component.ts
+++ SoftUni-Forum/src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  isRepassValid: boolean = false;
+
+  register(form: NgForm) {
+    if (!form) {
+      return;
+    }
+
+    if (form.invalid) {
+      console.log('Form is Invalid');
+      return;
+    }
+
+
+    if (form.value.password === form.value.rePassword) {
+
+      this.isRepassValid = true
+
+    } else {
+
+      return
+    }
+
+
+    console.log(form.value);
+    form.reset();
+  }
 
 }
