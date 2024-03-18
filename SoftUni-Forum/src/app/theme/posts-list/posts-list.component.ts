2aeded2dfbe72d57a7e3895a96ea90ea81e508a0
TimiPashov <pashov91@gmail.com>
2024-03-18 17:35:57 +0200

    implem onDestroy to posts list to cancel req

--- SoftUni-Forum/src/app/theme/posts-list/posts-list.component.ts
+++ SoftUni-Forum/src/app/theme/posts-list/posts-list.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from '../../api.service';
 import { Post } from '../../types/post';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
-export class PostsListComponent implements OnInit {
+export class PostsListComponent implements OnInit, OnDestroy {
+  private httpSubscribtion = {} as Subscription;
   posts: Post[] = [];
 
   constructor(private api: ApiService) { }
 
 
   ngOnInit(): void {
-    this.api.getPosts(5).subscribe(posts => {
+    this.httpSubscribtion = this.api.getPosts(5).subscribe(posts => {
       this.posts = posts;
+
     })
+  }
+
+  ngOnDestroy() { 
+    if (this.httpSubscribtion) {
+      this.httpSubscribtion.unsubscribe();
+    }
   }
 }
